0a318360a560b81b8cae0324ee8e8d0dad15a110
cicilzxstc <775514800@qq.com>
2024-10-27 21:20:18 +0800

    完成了Lab2：Sysinfo

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,16 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+//获取空闲内存
+void freebytes(uint64* dst) {
+    *dst = 0;
+    struct run* p = kmem.freelist;
+
+    acquire(&kmem.lock);		//添加锁，防止竞态
+    while (p) {
+        *dst += PGSIZE;			//统计空闲字节数
+        p = p->next;
+    }
+    release(&kmem.lock);
+}
