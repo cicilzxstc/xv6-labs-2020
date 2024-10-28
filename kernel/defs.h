875129cde2135aa3cf90d999437f965daa65800b
cicilzxstc <775514800@qq.com>
2024-10-28 19:42:28 +0800

    完成了Lab3：A kernel page table per process

--- kernel/defs.h
+++ kernel/defs.h
@@ -160,8 +160,8 @@ int             uartgetc(void);
 // vm.c
 void            kvminit(void);
 void            kvminithart(void);
-uint64          kvmpa(uint64);
-void            kvmmap(uint64, uint64, uint64, int);
+uint64          kvmpa(pagetable_t,uint64); //修改参数列表
+void            kvmmap(pagetable_t,uint64, uint64, uint64, int);  //修改参数列表
 int             mappages(pagetable_t, uint64, uint64, uint64, int);
 pagetable_t     uvmcreate(void);
 void            uvminit(pagetable_t, uchar *, uint);
@@ -179,6 +179,8 @@ int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
 int             vmprint(pagetable_t pagetable);         //打印页表内容函数声明
+pagetable_t     kvminit_newpgtbl();                     //声明新函数
+void            kvm_free_kernelpgtbl(pagetable_t);      //声明新函数
 
 // plic.c
 void            plicinit(void);
