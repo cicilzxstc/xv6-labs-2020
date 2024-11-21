961e325cfd9606b0e95950346ab16f58dbd9486a
cicilzxstc <775514800@qq.com>
2024-11-21 09:10:02 +0800

    完成了Lab8：Locks

--- kernel/buf.h
+++ kernel/buf.h
@@ -1,3 +1,16 @@
+// struct buf {
+//   int valid;   // has data been read from disk?
+//   int disk;    // does disk "own" buf?
+//   uint dev;
+//   uint blockno;
+//   struct sleeplock lock;
+//   uint refcnt;
+//   struct buf *prev; // LRU cache list
+//   struct buf *next;
+//   uchar data[BSIZE];
+// };
+
+// kernel/buf.h
 struct buf {
   int valid;   // has data been read from disk?
   int disk;    // does disk "own" buf?
@@ -5,8 +18,10 @@ struct buf {
   uint blockno;
   struct sleeplock lock;
   uint refcnt;
-  struct buf *prev; // LRU cache list
+  // struct buf *prev; // LRU cache list
+  struct buf *prev;
   struct buf *next;
   uchar data[BSIZE];
-};
 
+  uint timestamp;     //用于跟踪LRU-buf
+};
